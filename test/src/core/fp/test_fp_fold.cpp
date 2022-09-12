d6b1751c95b204691a34f00b400dbff9d686aa84
Mark Melton <mark.melton@notlem.com>
2022-09-12 14:50:31 -0400

    Removed dependence on util/common.h.

--- test/src/core/fp/test_fp_fold.cpp
+++ test/src/core/fp/test_fp_fold.cpp
@@ -2,14 +2,14 @@
 //
 
 #include <gtest/gtest.h>
-#include "core/util/common.h"
 #include "core/fp/fold.h"
 
 using namespace core::fp;
+using namespace std::literals::string_literals;
 
 TEST(Fp, FoldLeftEmpty)
 {
-    strings strs = { };
+    std::vector<std::string> strs = { };
     EXPECT_THROW(fold_l(strs, [](auto a, auto b) { return a + ":" + b; }),
 		 std::invalid_argument);
 }
@@ -17,21 +17,21 @@ TEST(Fp, FoldLeftEmpty)
 
 TEST(Fp, FoldLeftSingle)
 {
-    strings strs = { "abc" }; 
+    std::vector<std::string> strs = { "abc" }; 
     auto r = fold_l(strs, [](auto a, auto b) { return a + ":"s + b; });
     EXPECT_EQ(r, "abc");
 }
 
 TEST(Fp, FoldLeftMultipleStrings)
 {
-    strings strs = { "abc", "def", "ghi" };
+    std::vector<std::string> strs = { "abc", "def", "ghi" };
     auto r = fold_l(strs, [](auto a, auto b) { return a + ":"s + b; });
     EXPECT_EQ(r, "abc:def:ghi");
 }
 
 TEST(Fp, FoldLeftMultipleInts)
 {
-    ints values = { 1, 2, 3 };
+    std::vector<int> values = { 1, 2, 3 };
     {
 	auto r = fold_l(values, [](auto a, auto b) { return a + b; });
 	EXPECT_EQ(r, 6);
@@ -44,43 +44,43 @@ TEST(Fp, FoldLeftMultipleInts)
 
 TEST(Fp, FoldAny)
 {
-    auto r0 = any(vector<bool>{}, [](const auto& elem) { return elem; });
+    auto r0 = any(std::vector<bool>{}, [](const auto& elem) { return elem; });
     EXPECT_EQ(false, r0);
     
-    auto r1 = any(vector<bool>{false}, [](const auto& elem) { return elem; });
+    auto r1 = any(std::vector<bool>{false}, [](const auto& elem) { return elem; });
     EXPECT_EQ(false, r1);
     
-    auto r2 = any(vector<bool>{true}, [](const auto& elem) { return elem; });
+    auto r2 = any(std::vector<bool>{true}, [](const auto& elem) { return elem; });
     EXPECT_EQ(true, r2);
     
-    auto r3 = any(vector<bool>{false, true}, [](const auto& elem) { return elem; });
+    auto r3 = any(std::vector<bool>{false, true}, [](const auto& elem) { return elem; });
     EXPECT_EQ(true, r3);
     
-    auto r4 = any(vector<bool>{true, true}, [](const auto& elem) { return elem; });
+    auto r4 = any(std::vector<bool>{true, true}, [](const auto& elem) { return elem; });
     EXPECT_EQ(true, r4);
     
-    auto r5 = any(vector<bool>{false, false}, [](const auto& elem) { return elem; });
+    auto r5 = any(std::vector<bool>{false, false}, [](const auto& elem) { return elem; });
     EXPECT_EQ(false, r5);
 }
 
 TEST(Fp, FoldAll)
 {
-    auto r0 = all(vector<bool>{}, [](const auto& elem) { return elem; });
+    auto r0 = all(std::vector<bool>{}, [](const auto& elem) { return elem; });
     EXPECT_EQ(true, r0);
     
-    auto r1 = all(vector<bool>{false}, [](const auto& elem) { return elem; });
+    auto r1 = all(std::vector<bool>{false}, [](const auto& elem) { return elem; });
     EXPECT_EQ(false, r1);
     
-    auto r2 = all(vector<bool>{true}, [](const auto& elem) { return elem; });
+    auto r2 = all(std::vector<bool>{true}, [](const auto& elem) { return elem; });
     EXPECT_EQ(true, r2);
     
-    auto r3 = all(vector<bool>{false, true}, [](const auto& elem) { return elem; });
+    auto r3 = all(std::vector<bool>{false, true}, [](const auto& elem) { return elem; });
     EXPECT_EQ(false, r3);
     
-    auto r4 = all(vector<bool>{true, true}, [](const auto& elem) { return elem; });
+    auto r4 = all(std::vector<bool>{true, true}, [](const auto& elem) { return elem; });
     EXPECT_EQ(true, r4);
 
-    auto r5 = all(vector<bool>{false, false}, [](const auto& elem) { return elem; });
+    auto r5 = all(std::vector<bool>{false, false}, [](const auto& elem) { return elem; });
     EXPECT_EQ(false, r5);
 }
 
