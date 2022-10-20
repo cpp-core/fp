97f7db901640653ce5ba09a3913523d7bf9dedec
Mark Melton <mark.melton@notlem.com>
2022-10-20 13:36:19 -0400

    Updated join implementaiton to use new fp sequences.

--- src/tools/fp1.cpp
+++ src/tools/fp1.cpp
@@ -2,24 +2,9 @@
 //
 
 #include <iostream>
-#include <list>
-#include <set>
-#include <vector>
-#include "core/fp/seq.h"
-
-using core::fp::source;
-using namespace core::fp::chaining;
-using namespace std::string_literals;
 
 int main(int argc, const char *argv[]) {
-    std::vector<std::string> strs = { "abc", "def", "ghi" };
-    
-    auto r = source(strs).fold(""s, [](auto a, auto b) { return a + ":"s + b; });
-
-    for (auto n : r)
-	std::cout << n << " ";
-    std::cout << std::endl;
-
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
 
